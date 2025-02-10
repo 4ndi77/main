4c0f48eaf57a141d73777d18bdc300c7ed162ea4
4ndi77 <andivejseli10@gmail.com>
2025-02-10 12:16:15 +0100

    ads

---
+++ easy challange 29/java.js
@@ -0,0 +1,17 @@
+var students=['Andi', 'Loran', 'Klos', 'Diamant', 'Hello'];
+console.log(students[1]);
+
+students.push('Orli');
+console.log(students);
+
+students.pop();
+console.log(students);
+
+students.unshift('Jon');
+console.log(students);
+
+students.shift();
+console.log(students);
+
+students.splice(3,2 , 'AAAAA');
+console.log(students);
